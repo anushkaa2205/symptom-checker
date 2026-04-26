be355c681280050577e275fe7d7a08e79411cf9e
Harshita Mugdha <harshitamugdha@gmail.com>
2026-04-26 15:20:05 +0530

    fixed OAuth callback

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,37 +3,45 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+
 dotenv.config();
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
-},
-async (accessToken, refreshToken, profile, done) => {
-    try {
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
         const email = profile.emails[0].value;
 
         let user = await User.findOne({ email });
 
         if (!user) {
-            user = await User.create({
+          user = await User.create({
             Fname: profile.name.givenName,
             Lname: profile.name.familyName,
             email,
             googleId: profile.id
-            });
+          });
         }
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-            expiresIn: "7d"
-        });
+        const token = jwt.sign(
+          { id: user._id },
+          process.env.JWT_SECRET,
+          { expiresIn: "7d" }
+        );
 
         return done(null, {
-    token,
-    userId: user._id
-});
+          token,
+          userId: user._id
+        });
 
-    } catch (err) {
+      } catch (err) {
         return done(err, null);
+      }
     }
-}));
+  )
+);
